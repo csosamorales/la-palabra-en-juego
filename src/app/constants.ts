3023859730663b52d870f8a373bb2387d9e85f8a
Camila Sosa Morales <camisosa@fleek.co>
2025-03-31 00:07:06 -0300

    chore: use Guido's phone for whatsapp

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
 	whatsapp:
-		'https://api.whatsapp.com/send?phone=5492944624173&text=Hola!%20Quiero%20saber%20más%20sobre%20los%20turnos.',
+		'https://api.whatsapp.com/send?phone=5491141954559&text=Hola!%20Quiero%20saber%20más%20sobre%20los%20turnos.',
 	calendly:
 		'https://calendly.com/psicologalapalabraenjuego/30min?month=2023-10',
 	instagram: 'https://www.instagram.com/psicologalapalabraenjuego/',
