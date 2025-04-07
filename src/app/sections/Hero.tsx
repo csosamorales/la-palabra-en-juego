1d6ffbd9016f76d78a0e63e6eca3dbbbf59a88da
Camila Sosa Morales <camisosa@fleek.co>
2025-04-07 20:17:25 -0300

    fix: fix 1024 resolution

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -10,11 +10,11 @@ export const HeroSection: React.FC = () => (
 			alt="background image"
 			fill
 			priority
-			className="!w-auto !h-[80%] !top-[10%] lg:!top-0 lg:!h-[90vh] mr-auto ml-auto mt-16 mb-16 hidden md:inline -z-0 animate-fade animate-duration-[4500ms] animate-delay-500"
+			className="!w-auto sm:!-right-16 !h-[90%] lg:!top-0 xl:!h-[90%] mx-auto mt-16 mb-16 hidden md:inline -z-0 animate-fade animate-duration-[4500ms] animate-delay-500"
 		/>
 
 		<div className="flex flex-col justify-center items-center gap-1 animate-fade animate-once animate-duration-[1500ms] animate-ease-in">
-			<span className="text-3xl sm:text-4xl lg:text-[2.5rem] xl:text-6xl font-title">
+			<span className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-title">
 				La palabra en juego
 			</span>
 			<h1 className="text-lg sm:text-xl lg:text-2xl xl:text-3xl !font-text">
