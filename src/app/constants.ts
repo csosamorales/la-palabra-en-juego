bf5e59cbe91567a63df6abd88155e43de00dce9c
csosamorales <camilasosamorales@gmail.com>
2025-05-06 23:55:07 -0300

    chore: change calendly link

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,6 +2,6 @@ export const LINKS = {
 	whatsapp:
 		'https://api.whatsapp.com/send?phone=5491157087201',
 	calendly:
-		'https://calendly.com/lapalabraenjuego/15min',
+		'https://calendly.com/guidolv/meet-up',
 	instagram: 'https://www.instagram.com/lapalabraenjuego/',
 };
