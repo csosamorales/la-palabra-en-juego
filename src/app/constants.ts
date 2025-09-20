df927ca0d3df70480044cd991c4eaf42b0bd42f6
csosamorales <camilasosamorales@gmail.com>
2025-09-20 17:46:15 -0300

    chore: update whatsapp message

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
 	whatsapp:
-		'https://api.whatsapp.com/send?phone=5491157087201&text=Hola,%20quiero%20consultar%20para%20comenzar%20terapia.%20Mi%20nombre%20es%20[NOMBRE],%20tengo%20[EDAD]%20años%20y%20mis%20horarios%20disponibles%20son%20[DÍAS%20/%20HORAS]',
+		'https://api.whatsapp.com/send?phone=5491157087201&text=Hola,%20quiero%20consultar%20para%20comenzar%20terapia.%20Mi%20nombre%20es%20...,%20tengo%20...%20años%20y%20mis%20horarios%20disponibles%20son%20...',
 	calendly:
 		'https://calendly.com/guidolv/meet-up',
 	instagram: 'https://www.instagram.com/lapalabraenjuego/',
